cdbc5f1b150d6c9fc0874024efea3dcc9119cca6
Valentin <qweqweq@qwe>
2021-07-19 21:51:04 +0300

    initialization

---
+++ src/main/java/com/vvvxdd/project_management_java/ProjectManagementJavaApplication.java
@@ -0,0 +1,13 @@
+package com.vvvxdd.project_management_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vvvxdd/project_management_java/ProjectManagementJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvvxdd.project_management_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
